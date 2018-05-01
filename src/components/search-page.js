af14bdd6bcf0323a8cde8527527bf5ec7ec5177b
tomdaniels <thomsondaniels@hotmail.com>
2018-05-01 12:31:43 +1000

    search movie DB based on user input on select

--- src/components/search-page.js
+++ src/components/search-page.js
@@ -1,30 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
 
 class SearchPage extends React.Component {
   state = {
     options: [
-      { label: 'apple', value: 'apple' },
+      { label: 'apple pie', value: 'apple pie' },
       { label: 'banana', value: 'banana' },
       { label: 'pear', value: 'pear' }
     ],
     items: [],
     value: '',
+    movies: [],
   };
 
   handleChange = (e) => {
-    const value = e.target.value;
+    const value = e.value;
     this.setState(() => ({
       value,
     }));
   };
 
-  handleSelect = (e) => {
-
+  getMoviesByInput(input, key) {
+    const encodedInput = input.replace(' ', '+');
+    const endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${encodedInput}`;
+    axios.get(endpoint).then((res) => {
+      const results = res.data.results;
+      const movie = results.map((result) => ({
+        id: result.id,
+        title: result.title,
+        date: result.release_date,
+        descriptions: result.overview,
+        image: result.poster_path,
+      }));
+      console.log(movie.map((mov) => mov));
+    }).catch((error) => console.log(error));
   }
 
   render() {
+    this.getMoviesByInput(this.state.value, this.props.apiKey);
     return (
       <div>
         <h1>Search</h1>
@@ -32,10 +48,17 @@ class SearchPage extends React.Component {
           options={this.state.options}
           inputValue={this.state.value}
           onChange={this.handleChange}
+          value={this.state.value}
+          allowCreate
+          clearable
         />
       </div>
     )
   }
+};
+
+SearchPage.propTypes = {
+  apiKey: PropTypes.string.isRequired,
 };
 
 export default SearchPage;
