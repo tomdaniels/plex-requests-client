397ca2eb20de0717c9294dde8461ee4dd46f975d
tomdaniels <thomsondaniels@hotmail.com>
2018-05-03 09:33:22 +1000

    class based component and margin fix

--- src/components/tv-list-item.js
+++ src/components/tv-list-item.js
@@ -1,38 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TvListItem = ({ id, title, desc, date, image }) => {
-
-  const onClick = () => {
-    console.log(id)
+class TvListItem extends React.Component {
+  state ={
+    exandTvShow: false,
   };
 
-  return (
-    <div>
-      <div className="series-list__title">
-        <h5>{title}</h5>
-        <h5>{date}</h5>
+  showExpandClick = () => {
+    this.setState(() => ({
+      expandTvShow: true,
+    }));
+  };
+
+  onFullSeriesClick = () => {
+    console.log(this.props.id)
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="series-list__title">
+          <h5>{this.props.title}</h5>
+          <h5>{this.props.date}</h5>
+        </div>
+        <div className="series-list__description">
+          <img className="series-list__poster" src={this.props.image} alt={`Series Poster, ${this.props.title}`} />
+          <p>{this.props.desc || 'Sorry, there is no description available for this series.'}</p>
+        </div>
+        <div className="series-list__button-wrap">
+          <button
+            className="series-list__button"
+            onClick={this.onFullSeriesClick}
+          >
+            Request Entire Series
+          </button>
+          <button
+            className="series-list__button"
+            onClick={this.showExpandClick}
+          >
+            +
+          </button>
+        </div>
       </div>
-      <div className="series-list__description">
-        <img
-        className="series-list__poster"
-          src={image}
-          alt={`Series Poster, ${title}`}
-        />
-        <p>{desc || 'Sorry, there is no description available for this series.'}</p>
-      </div>
-      <div className="series-list__button-wrap">
-        <button
-          className="series-list__button"
-          onClick={onClick}
-        >
-          Request
-        </button>
-        <button className="series-list__expand-button">+</button>
-      </div>
-    </div>
-  )
+    )
+  }
 };
+
+
 
 TvListItem.propTypes = {
   show: PropTypes.shape({
