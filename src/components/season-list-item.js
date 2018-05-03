6d588904cafbe02f55c3730f37fd14de8cbfc066
tomdaniels <thomsondaniels@hotmail.com>
2018-05-03 14:56:20 +1000

    log id on user click for individual season

--- src/components/season-list-item.js
+++ src/components/season-list-item.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const SeasonListItem = ({ id, date, name, epCount }) => (
-  <div className="plex-requests__season-list">
-    <p>{name} ({epCount === 1 ? '1 episode' : `${epCount} episodes`}) - {date}</p>
-  </div>
-);
+const SeasonListItem = ({ id, date, name, epCount }) => {
+
+  const onClick = () => {
+    console.log(id)
+  }
+
+  return (
+    <div className="plex-requests__season-list-item">
+      <div>{name} ({epCount === 1 ? '1 episode' : `${epCount} episodes`}) - {date}</div>
+      <button onClick={onClick}>Request Season</button>
+    </div>
+  )
+};
 
 export default SeasonListItem;
