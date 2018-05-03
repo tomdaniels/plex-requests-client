a10d36842e8d61116bd7ce265a133f0240c90d52
tomdaniels <thomsondaniels@hotmail.com>
2018-05-03 16:38:24 +1000

    refactor redundant files

--- src/test/components/dashboard.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Dashboard from '../../components/dashboard';
-
-test('should render the dashboard page', () => {
-    const wrapper = shallow(<Dashboard />);
-    expect(wrapper).toMatchSnapshot();
-});
--- src/test/components/loading-page.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import LoadingPage from '../../components/loading-page';
-
-test('should render the dashboard page', () => {
-    const wrapper = shallow(<LoadingPage />);
-    expect(wrapper).toMatchSnapshot();
-});
