c63eac0c7075493dc6aff3019401efaddf14a8f6
tomdaniels <thomsondaniels@hotmail.com>
2018-05-16 16:14:49 +1000

    plugin api to handle individual seasons

--- src/components/season-list-item.js
+++ src/components/season-list-item.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 
 const SeasonListItem = ({ id, date, name, epCount, seriesId }) => {
 
   const onClick = () => {
-    alert(`TODO: finish https://github.com/tomdaniels/plex-requests-api to handle: series id ${seriesId}, season id ${id}`);
+    console.log('here');
+    const endpoint = `http://requests-api.tomd.io/v1/tv/${id}/season/${seriesId}`;
+    axios.post(endpoint).then((response) => {
+      if (response.status === 200) {
+        alert(`${this.props.title} successfully requested`);
+      }
+    }).catch((error) => {
+      console.log(error);
+      alert(`oops! Call the tech guys.. something went wrong: ${error}`);
+    });
   }
 
   return (
