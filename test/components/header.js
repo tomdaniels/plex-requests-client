ab0bb675695470de11c1b9034fdb739b5e49d1eb
tomdaniels <thomsondaniels@hotmail.com>
2018-06-02 12:46:18 +1000

    testing header component

---
+++ test/components/header.js
@@ -0,0 +1,21 @@
+import Header from '../../src/components/header';
+
+const shallow = enzyme.shallow;
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<Header title="Plex Requests" />);
+});
+
+describe('Header.js', () => {
+  it('should render the header section', () => {
+    expect(wrapper.find('.plex-requests__header-wrap')).to.be.present();
+  });
+  it('should render a small image icon with anchor element to click on', () => {
+    expect(wrapper.find('.plex-requests__header-img')).to.have.length(1);
+  });
+  it('should display the title passed in via props', () => {
+    expect(wrapper.find('.plex-requests__header-title')).to.have.text('Plex Requests');
+  });
+});
