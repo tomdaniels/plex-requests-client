53a6de9508bad1925036b670f1c30217b4d20ca6
tomdaniels <thomsondaniels@hotmail.com>
2018-08-19 13:27:02 +1000

    refactor: duplication hadnling logic

--- src/components/media-list.js
+++ src/components/media-list.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import faker from 'faker';
 import remove from 'lodash/remove';
 import ScrollAppear from './scroll-appear';
 import MediaListItem from './media-list-item';
 
 const MediaList = ({ media, apiKey }) => {
-  media
-    .sort((a, b) => b.popularity - a.popularity)
-    .filter((item, index, self) => index == self.indexOf(item));
+  const fitleredMedia = (
+    media
+      .sort((a, b) => b.popularity - a.popularity)
+      .filter(item => remove(media, duplicate => duplicate.id === item.id))
+    );
 
   return (
     <div>
       <h4 className="media-list__section-header">
-        {media.length === 1 ? `1 result found` : `${media.length} results found`}
+        {fitleredMedia.length === 1 ? `1 result found` : `${fitleredMedia.length} results found`}
       </h4>
       <ul>
         {
-          media.length > 0 && media.map((mediaItem) => (
+          fitleredMedia.length > 0 && fitleredMedia.map((mediaItem) => (
             <li key={mediaItem.id} className="plex-requests__media-list">
               <ScrollAppear
-                direction="left"
+                direction={faker.random.arrayElement(["left","right"])}
                 children={ <MediaListItem apiKey={apiKey} {...mediaItem} /> }
               >
               </ScrollAppear>
