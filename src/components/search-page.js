77afd797b8b76da58b44a513a7c40c4c0e78dec8
tomdaniels <thomsondaniels@hotmail.com>
2018-08-30 20:54:28 +1000

    feat: clear search input

--- src/components/search-page.js
+++ src/components/search-page.js
@@ -6,6 +6,7 @@ import MediaList from './media-list';
 class SearchPage extends React.Component {
   state = {
     media: [],
+    searchInput: '',
     showList: false,
   };
 
@@ -55,6 +56,10 @@ class SearchPage extends React.Component {
     e.preventDefault();
     const input = e.target.value;
 
+    this.setState(() => ({
+      searchInput: input,
+    }));
+
     if (input.length >= 4) {
       this.setState(() => ({
         showList: true,
@@ -71,6 +76,7 @@ class SearchPage extends React.Component {
   clearList = () => {
     this.setState(() => ({
       media: [],
+      searchInput: '',
       showList: false,
     }));
   };
@@ -97,6 +103,7 @@ class SearchPage extends React.Component {
           <input
             className="search-page__text-input"
             type="text"
+            value={this.state.searchInput}
             placeholder="by movie or show title.."
             onChange={this.handleChange}
           />
