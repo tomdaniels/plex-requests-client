55eb1865a7b6f4bd854191693a837200c580c024
tomdaniels <thomsondaniels@hotmail.com>
2018-09-10 10:43:34 +1000

    chore: update deps

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const { whyDidYouUpdate } = require('why-did-you-update');
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV !== 'production') {
+  whyDidYouUpdate(React, {
+    groupByComponent: true,
+    collapseComponentGroups: false
+  });
+}
 
 app.use(express.static(publicPath));
 
