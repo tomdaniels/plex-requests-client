dd3bb458d989255b671619cfd1a7bccd5866a688
tomdaniels <thomsondaniels@hotmail.com>
2018-09-20 19:22:49 +1000

    fix: season list lint errors

--- src/components/seasons-list.js
+++ src/components/seasons-list.js
@@ -3,32 +3,31 @@ import PropTypes from 'prop-types';
 import SeasonListItem from './season-list-item';
 
 const Seasons = ({ title, seasons }) => {
-
   const seasonsList = seasons.reverse();
   return (
     <div>
-      <h4 className="season-list__section-header">
-        {title}
-      </h4>
-      <h6 className="season-list__section-subheader">{seasons.length === 1 ? '1 Season found' : `${seasons.length} seasons found`}</h6>
+      <h4 className="season-list__section-header">{title}</h4>
+      <h6 className="season-list__section-subheader">
+        {seasons.length === 1
+          ? '1 Season found'
+          : `${seasons.length} seasons found`}
+      </h6>
       <ul className="season-list__wrap">
-        {
-          seasonsList.length > 0 ? (seasons.map((season) => (
-            <li key={season.id} className="plex-requests__seasons-list">
-              <SeasonListItem title={title} {...season} />
-            </li>
-          ))) : (
-            'No results found..'
-          )
-        }
+        {seasonsList.length > 0
+          ? seasons.map(season => (
+              <li key={season.id} className="plex-requests__seasons-list">
+                <SeasonListItem title={title} {...season} />
+              </li>
+            ))
+          : 'No results found..'}
       </ul>
     </div>
-  )
+  );
 };
 
 Seasons.propTypes = {
   title: PropTypes.string.isRequired,
-  seasons: PropTypes.array.isRequired,
+  seasons: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
 };
 
 export default Seasons;
