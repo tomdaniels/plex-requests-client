528b78c64eddb02a18d91cae7e3cfca2d467f0e1
tomdaniels <thomsondaniels@hotmail.com>
2018-09-20 18:47:01 +1000

    fix: media-list.js lint errors

--- src/components/media-list.js
+++ src/components/media-list.js
@@ -1,50 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import faker from 'faker';
-import remove from 'lodash/remove';
 import ScrollAppear from './scroll-appear';
 import MediaListItem from './media-list-item';
 
+const direction = faker.random.arrayElement(['left', 'right']);
+
 const MediaList = ({ media, apiKey }) => {
-  const fitleredMedia = (
-    media
-      .filter((item, index, self) => (
-        index === self.findIndex((t) => (
-          t.title === item.title && t.id === item.id
-        ))
-      ))
-      .sort((a, b) => b.popularity - a.popularity)
-    );
+  const fitleredMedia = media
+    .filter(
+      (item, index, self) =>
+        index ===
+        self.findIndex(
+          element => element.title === item.title && element.id === item.id,
+        ),
+    )
+    .sort((first, second) => second.popularity - first.popularity);
 
   return (
     <div>
       <h4 className="media-list__section-header">
-        {fitleredMedia.length === 1 ? `1 result found` : `${fitleredMedia.length} results found`}
+        {fitleredMedia.length === 1
+          ? `1 result found`
+          : `${fitleredMedia.length} results found`}
       </h4>
       <ul>
-        {
-          fitleredMedia.length > 0 && fitleredMedia.map((mediaItem) => (
+        {fitleredMedia.length > 0 &&
+          fitleredMedia.map(mediaItem => (
             <li key={mediaItem.id} className="plex-requests__media-list">
-              <ScrollAppear
-                direction={faker.random.arrayElement(["left","right"])}
-                children={ <MediaListItem apiKey={apiKey} {...mediaItem} /> }
-              >
+              <ScrollAppear direction={direction}>
+                <MediaListItem apiKey={apiKey} {...mediaItem} />
               </ScrollAppear>
             </li>
-          ))
-        }
+          ))}
       </ul>
     </div>
   );
-}
+};
 
 MediaList.propTypes = {
   apiKey: PropTypes.string.isRequired,
-  media: PropTypes.arrayOf(PropTypes.objectOf(
-    PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.number
-      ])))
+  media: PropTypes.arrayOf(
+    PropTypes.objectOf(
+      PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    ),
+  ),
 };
 
 MediaList.defaultProps = {
