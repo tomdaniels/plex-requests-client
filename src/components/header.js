45a9fe568bacea8944da3168a9925967d58ae318
tomdaniels <thomsondaniels@hotmail.com>
2018-09-20 19:21:46 +1000

    fix: header lint

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Header = ({title}) => (
-    <header>
-      <div className="plex-requests__header-wrap">
-        <div className="plex-requests__header-left">
-          <img
-              className="plex-requests__header-img"
-              src="./images/play-symbol.png"
-              alt="Plex Requests for tomd.io" />
-            <a className="plex-requests__header-title" href="http://requests.tomd.io">{title}</a>
-        </div>
+const Header = ({ title }) => (
+  <header>
+    <div className="plex-requests__header-wrap">
+      <div className="plex-requests__header-left">
+        <img
+          className="plex-requests__header-img"
+          src="./images/play-symbol.png"
+          alt="Plex Requests for tomd.io"
+        />
+        <a
+          className="plex-requests__header-title"
+          href="http://requests.tomd.io"
+        >
+          {title}
+        </a>
       </div>
-    </header>
+    </div>
+  </header>
 );
 
 Header.propTypes = {
